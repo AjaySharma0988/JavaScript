49c02f735ce273a572b823e1748a999a566d5021
Ajay Sharma <ajsharma0988@gmail.com>
2024-07-01 12:13:06 +0000

    convergence of datatype

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score ="33w"
+
+//console.log(typeof score);
+// console.log(typeof(score))
+
+let numberin = Number(score)
+
+// console.log(typeof numberin);
+// console.log(numberin);
+
+// "33"= 33
+// "33aas"=> NaN
+// True =>1; False =>0;
+
+// let isloggedIn = "" // is falles
+// let isloggedIn = "cg" // is True
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
