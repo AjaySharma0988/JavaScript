9ad8c11c774a4386fddcea10d0d68a1649511785
Ajay Sharma <ajsharma0988@gmail.com>
2024-07-01 13:31:14 +0000

    conversionoperation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -15,4 +15,28 @@ let numberin = Number(score)
 // let isloggedIn = "" // is falles
 // let isloggedIn = "cg" // is True
 let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// ****************** operations ********************
+
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 ="ajay"
+let str2 =" Sharma"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1"+2); => 12
+// console.log(1 + "2"); => 12
+// console.log("1" + 2 + 2); => 122
+// console.log(1 + 2 + "2"); => 32
+
