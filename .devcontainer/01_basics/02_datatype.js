452277cca7bcbe696ab558403419f3e65300f6a4
Ajay Sharma <ajsharma0988@gmail.com>
2024-07-02 17:43:51 +0000

    datatype

--- .devcontainer/01_basics/02_datatype.js
+++ .devcontainer/01_basics/02_datatype.js
@@ -13,5 +13,26 @@ let undefined;
 // undefined => 
 // symbol => unique
 
-console.log(typeof undefined);// undefined
-console.log(typeof null);// bjeect
+//console.log(typeof undefined);// undefined
+//console.log(typeof null);// bjeect
+
+//*****************Memory Type**********************
+
+//Stack (primitive), Heap (non-primitive)
+
+// stack ex- gets copy
+// let myName = "ajay"
+
+// let anotherName = "aju"
+// console.log(anotherName);
+// console.log(myName);
+
+//Heap ex- gets refrence 
+// let stdOne = {
+//         email : "ajaysharma@gmail.com",
+//         upi :"12344"
+// }
+// let stdTwo = stdOne;
+// stdOne.email = "ajay@gmail.com";
+// console.log(stdTwo);
+// console.log(stdOne);
