98d005581a3cd8900806f3a843da614c5deffaf9
Ajay Sharma <ajsharma0988@gmail.com>
2024-07-03 05:43:09 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,24 @@
+const name = "ajaybharatwage"
+const repoCount = 50
+
+// console.log(name + repoCount + "sharma"); //old method 
+// console.log(`My Name IS ${name} and repoCount is ${repoCount}`) //New Method is Called StringInterpolation
+
+
+const gameName = new String('ajaysharma')
+
+// console.log(gameName.charAt(1)) //to find the curecter with number
+// console.log(gameName.indexOf('y')) //to find the numbcolsole
+
+const newString = gameName.substring(0,3)
+//console.log(newString)
+const anotherString = gameName.slice(-9,4)
+//console.log(anotherString)
+const newStringOne = '   yaja   '
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+const url="https://ajay.com/ajay%20sharma"
+//console.log(url.replace('%20','-'))
+//console.log(url.includes('ajay')) // for chacking it is present in string or not 
+const newSplite = 'ajay-sharma'
+//console.log(newSplite.split('-'));
