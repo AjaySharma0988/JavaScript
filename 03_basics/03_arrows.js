3190b4e028b4e00e6f3828c6e5d50370d29bac8a
AjaySharma0988 <ajsharma0988@gmail.com>
2024-07-04 16:40:10 +0000

    Arrow

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -33,10 +33,15 @@ const chai = () => { //now it is arrow function
 }
 //chai()
 
-//() => {} // *basic stuctuer of arrow function
+//() => {} // *basic syntex of arrow function
 // const addTwo = (num1, num2) => { //basic arrow function
-//     return num1 + num2
+//     return num1 + num2 //*** if carlybrases use need to use "return"
 // }
 
-const addTwo = (num1, num2) => (return num1 + num2)//called 
+//const addTwo = (num1, num2) => num1 + num2 //called Implicitly it means that the JavaScript engine handles it automatically without requiring explicit instructions from you.
+
+//const addTwo = (num1, num2) => (num1 + num2)//no require of return
+
+const addTwo = (num1, num2) => ({userName:"Ajay"})//+*+* to return object in arrow function the object must raped it into "()"
+//Another symtex of arrow () => ()
 console.log(addTwo(6,9))
