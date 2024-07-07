df1b73bd97362d845d336c84e446f63050ae138d
AjaySharma0988 <ajsharma0988@gmail.com>
2024-07-07 09:34:27 +0000

    do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while
+let index = 1
+
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 1
+}
+
+let myArray = ["spiderman","superman","ironMan"]
+let arr = 0
+while(  arr < myArray.length ){
+// console.log(`value is ${myArray[arr]}`);
+    arr=arr+1
+}
+
+// do while loop
+
+let score = 11
+
+do {
+    console.log(`the score is ${score}`);
+    score++
+} while (score <=10);
